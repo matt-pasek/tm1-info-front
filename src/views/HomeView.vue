abe858055d0e67ec15a49ef38b85f68ff7b8135b
matt-pasek <mpasek@tm1.edu.pl>
2024-03-23 17:08:47 +0100

    code cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Home</h1>
 </template>
+
+<script setup lang="ts"></script>
